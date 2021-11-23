dfa684373e647c891fe0900031f3df857f5c35a2
Christian Plum <christian.plum2@alumni.fh-aachen.de>
2021-11-23 21:51:16 +0100

    Enums hinzugefügt

---
+++ Ticketautomat/Classes/EStationZone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketautomat.Classes
+{
+    enum EStationZone
+    {
+        ZONE_A,
+        ZONE_B,
+        ZONE_C
+    }
+}
---
+++ Ticketautomat/Classes/ETariffLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketautomat.Classes
+{
+    enum ETariffLevel
+    {
+        TARIFF_A,
+        TARIFF_B,
+        TARIFF_C
+    }
+}
