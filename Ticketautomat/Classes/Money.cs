8293e383163ce84385aa41acfa7507e511dddf30
Louis Hombach <louis.hombach@alumni.fh-aachen.de>
2021-11-24 01:29:43 +0100

    Money, PriceEntry und StationGraph hinzugefügt

---
+++ Ticketautomat/Classes/Money.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketautomat.Classes
+{
+    class Money
+    {
+        /// <summary>
+        /// Geldbetrag
+        /// </summary>
+        private float m_value;
+        /// <summary>
+        /// Art des Geldes (Scheine oder Münzen)
+        /// </summary>
+        private EMoneyType m_moneyType;
+        
+        public EMoneyType MoneyType { get => m_moneyType; set => m_moneyType = value; }
+        public float Value { get => m_value; set => m_value = value; }
+    }
+}
