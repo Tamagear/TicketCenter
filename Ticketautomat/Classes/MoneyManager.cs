ef52407072ce27bcce9a62397a01d006b8c0a8c6
Harutoro <Harutoro@users.noreply.github.com>
2021-11-24 09:44:10 +0100

    Erstellen von MoneyManager

--- Ticketautomat/Classes/MoneyManager.cs
+++ Ticketautomat/Classes/MoneyManager.cs
@@ -8,5 +8,24 @@ namespace Ticketautomat.Classes
 {
     class MoneyManager
     {
+        private IEnumerable<Money> m_moneyFillState;
+
+        private int m_ticketPaperLeft;
+
+        private IEnumerable<Money> m_currentlyInsertedMoney;
+
+
+        public InsertMoney(p_money)
+        {
+            return p_wert;
+        }
+        public GetChange()
+        {
+            return p_money;
+        }
+        public void CancelMoneyInsertion()
+        {
+            
+        }
     }
 }
